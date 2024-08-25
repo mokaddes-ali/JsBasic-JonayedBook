ab9753d9331969f33bccdb1c924e2a20a805df7b
mokadde <mokaddes.ru2000@gmail.com>
2024-08-25 07:58:38 +0600

    commmit

---
+++ object.js
@@ -0,0 +1,9 @@
+var rahim ={
+    name: 'Rahim Uddin',
+    age: 21,
+    job: 'Student',
+    phone: 01700000000,
+    email: 'mokaddes.2000@gmail.com',
+    address: 'Dhaka, Bangladesh'
+}
+console.log(rahim);
