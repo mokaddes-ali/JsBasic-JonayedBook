83a4d0c28cd05044f57705f75382806a1ec4ab36
mokadde <mokaddes.ru2000@gmail.com>
2025-02-11 14:50:06 +0600

    array commit

--- object.js
+++ object.js
@@ -1,11 +1,11 @@
-var rahim ={
-    name: 'Rahim Uddin',
-    age: 21,
-    job: 'Student',
-    phone: 01700000000,
-    email: 'mokaddes.2000@gmail.com',
-    address: 'Dhaka, Bangladesh'
-}
+// var rahim ={
+//     name: 'Rahim Uddin',
+//     age: 21,
+//     job: 'Student',
+//     phone: 01700000000,
+//     email: 'mokaddes.2000@gmail.com',
+//     address: 'Dhaka, Bangladesh'
+// }
 
-rahim.phone = 01700000001;
-console.log(rahim);
+// rahim.phone = 01700000001;
+// console.log(rahim);
