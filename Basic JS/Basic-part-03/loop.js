4bf73251f7c8e9cc8ae43662d7d8916d4b201e72
Mokaddes-Ali <mokaddes.ru2000@gmail.com>
2026-01-08 21:35:24 +0600

    practice loop javascript commit

--- Basic JS/Basic-part-03/loop.js
+++ Basic JS/Basic-part-03/loop.js
@@ -1,6 +1,25 @@
 //! Loop => for, while, do while loop
 //? For Loop
 //* Structure for ( initialization; condition; update ){ //body   }
+//! count last value 11 than stop loop
+let sum = 0;
 for ( let count = 1; count<= 10; count++) {
-    console.log("Bangladesh");
+    sum = sum + count;
+    console.log("Sum: ", sum);
+    console.log("Bangladesh", count);
 }
+ console.log(`Sum: ${sum}`);
+
+ //! Example -02
+ //* 1-100 odds number sum
+ //? 1 + 3 + 5 +----------
+ let sumEven = 0;
+ for ( let count1 = 1; count1 <= 100; count1 = count1 + 2 ){
+    sumEven = sumEven + count1;
+    console.log(`1 to 100 even sum: ${sumEven}`);
+ }
+  console.log(`Total sum: ${sumEven}`);
+
+//! Example -03
+ //* 1-100 even number sum
+   //? 2 + 4 + 6 + 8 +
